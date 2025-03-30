b15e045434fbcaa7878abf71905efde435c09e72
annagholt <anna.holt@protonmail.com>
2025-03-30 12:49:03 +0200

    rotate array

---
+++ 189/189.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k %= n  # Handle cases where k > n
+        
+        # Reverse the entire array
+        nums.reverse()
+        
+        # Reverse the first k elements properly
+        nums[:k] = list(reversed(nums[:k]))
+        
+        # Reverse the remaining elements properly
+        nums[k:] = list(reversed(nums[k:]))
+
+nums = [1, 2, 3, 4, 5, 6, 7]
+k = 3
+print("Initial array: ", nums, "rotation factor: ", k)
+Solution().rotate(nums, k)
+print("Rotated array: ", nums)  # Output: [5, 6, 7, 1, 2, 3, 4]
