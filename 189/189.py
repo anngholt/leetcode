fc4a42f15f174a8f88906b8f1e64981f085a137c
annagholt <anna.holt@protonmail.com>
2025-03-30 13:15:52 +0200

    ensure script is executed directly

--- 189/189.py
+++ 189/189.py
@@ -17,8 +17,10 @@ class Solution(object):
         # Reverse the remaining elements properly
         nums[k:] = list(reversed(nums[k:]))
 
-nums = [1, 2, 3, 4, 5, 6, 7]
-k = 3
-print("Initial array: ", nums, "rotation factor: ", k)
-Solution().rotate(nums, k)
-print("Rotated array: ", nums)  # Output: [5, 6, 7, 1, 2, 3, 4]
+# Ensure this only runs when the script is executed directly
+if __name__ == "__main__":
+    nums = [1, 2, 3, 4, 5, 6, 7]
+    k = 3
+    print("Initial array: ", nums, "rotation factor: ", k)
+    Solution().rotate(nums, k)
+    print("Rotated array: ", nums)  # Output: [5, 6, 7, 1, 2, 3, 4]
